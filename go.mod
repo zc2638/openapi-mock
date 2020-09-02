be0621402b5459b8c93029e261a803b0827c3814
zc <zc2638@qq.com>
2020-09-02 14:55:46 +0800

    添加Asia/shanghai时区

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module mock
 
-go 1.13
+go 1.15
 
 require (
 	github.com/boltdb/bolt v1.3.1
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	_ "mock/config"
 	_ "mock/listener"
 	"mock/route"
+	"time"
+	_ "time/tzdata"
 )
 
 /**
  * Created by zc on 2019-10-24.
  */
 func main() {
+	location, err := time.LoadLocation("Asia/Shanghai")
+	if err == nil {
+		time.Local = location
+	} else {
+		fmt.Println("load location Asia/Shanghai error: ", err)
+	}
 	route.Start()
 }
